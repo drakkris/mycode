0ecaa5d1aee0b9d0bf7bf03a6d51125ec33aa3a4
drakkris <kristi.drake@outlook.com>
2024-07-02 17:38:06 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+#display only the IP addresses to the screen
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+#display IP addresses using f-string
+print(f"IP addresses: {iplist[3]} and {iplist[4]}")
+
+#display port number
+#print("The second item in the list (port): " + str(my_list[1]) )
+
+#display port status
+#print("The last item in the list (state): " + my_list[2] )
+
+
